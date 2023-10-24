098d041740b389b7762f76eb505ef47e15ea3c2a
Mitantsoaa <mitaranaivo@gmail.com>
2023-10-24 06:48:23 +0300

    Pagination en cours

--- List.php
+++ List.php
@@ -3,7 +3,18 @@ include 'Connexion.php';
 include 'functions.php';
 include 'header.php';
 $result = getAllData($connexion);
+if(isset($_GET['page']) && !empty($_GET['page'])){
+    $currentPage = (int) strip_tags($_GET['page']);
+}else{
+    $currentPage = 1;
+}
 
+$parPage = 10;
+$page = getPersNumber($connexion);
+$p = (int)$page[0];
+$pages = ceil($p / $parPage);
+$premier = ($currentPage * $parPage) - $parPage;
+$perpage = mysqli_query($connexion, "SELECT * FROM personnel LIMIT ".$premier.",".$parPage);
 ?>
 <div class="container">
 <table class="table">
@@ -15,29 +26,39 @@ $result = getAllData($connexion);
         <th></th>
     </tr>
     <?php
-    while($ligne = mysqli_fetch_assoc($result)){
+    while($ligne = mysqli_fetch_assoc($perpage)){
     ?>
     <tr>
      <td><img src="image/<?php echo $ligne['img_url'] ;?>"/></td>
      <td><?php echo $ligne['nom'] ;?></td>
      <td><?php echo $ligne['email'] ;?></td>
      <td><?php echo $ligne['phone'] ;?></td>
-     <td><a href="http://localhost/DTC/Eval/eval-php-bdd-Mitantsoaa/Details.php?id=<?php echo $ligne['id'];?>">Voir</a></td>
-     <td><a href="http://localhost/DTC/Eval/eval-php-bdd-Mitantsoaa/update.php?id=<?php echo $ligne['id'];?>">Update</a></td>
-     <td><a href="http://localhost/DTC/Eval/eval-php-bdd-Mitantsoaa/delete.php?id=<?php echo $ligne['id'];?>">Delete</a></td>
+     <td><a href="/Details.php?id=<?php echo $ligne['id'];?>">Voir</a></td>
+     <td><a href="/update.php?id=<?php echo $ligne['id'];?>">Update</a></td>
+     <td><a href="/delete.php?id=<?php echo $ligne['id'];?>">Delete</a></td>
     </tr>
     <?php
 }
-if(isset($_GET['page']) && !empty($_GET['page'])){
-    $currentPage = (int) strip_tags($_GET['page']);
-}else{
-    $currentPage = 1;
-}
 
-$parPage = 10;
-$page = getPersNumber($connexion);
-
-$pages = ceil($page[0] / $parPage);
 ?>
 </table>
+
+<nav>
+    <ul class="pagination">
+        <!-- Lien vers la page précédente (désactivé si on se trouve sur la 1ère page) -->
+        <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
+            <a href=".List.php/?page=<?= $currentPage - 1 ?>" class="page-link">Précédente</a>
+        </li>
+        <?php for($page = 1; $page <= $pages; $page++): ?>
+            <!-- Lien vers chacune des pages (activé si on se trouve sur la page correspondante) -->
+            <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
+                <a href=".List.php/?page=<?= $page ?>" class="page-link"><?= $page ?></a>
+            </li>
+        <?php endfor ?>
+            <!-- Lien vers la page suivante (désactivé si on se trouve sur la dernière page) -->
+            <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
+            <a href=".List.php/?page=<?= $currentPage + 1 ?>" class="page-link">Suivante</a>
+        </li>
+    </ul>
+</nav>
     </div>

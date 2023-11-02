cb369494dd44c64f745a4237b2892c02f2cde5d7
Mitantsoaa <mitaranaivo@gmail.com>
2023-11-02 21:44:34 +0300

    pagination en place

--- List.php
+++ List.php
@@ -42,22 +42,27 @@ $perpage = mysqli_query($connexion, "SELECT * FROM personnel LIMIT ".$premier.",
 
 ?>
 </table>
+<?php 
+ for($pages = 1; $pages<= $p; $pages++) {  
 
+        echo '<a href = "list.php?page=' . $pages . '">' . $pages . ' </a>';  
+
+    }  ?>
 <nav>
     <ul class="pagination">
         <!-- Lien vers la page précédente (désactivé si on se trouve sur la 1ère page) -->
         <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
-            <a href="http://localhost:8000/.List.php/?page=<?= $currentPage - 1 ?>" class="page-link">Précédente</a>
+            <a href="list.php/?page=<?= $currentPage - 1 ?>" class="page-link">Précédente</a>
         </li>
         <?php for($page = 1; $page <= $pages; $page++): ?>
             <!-- Lien vers chacune des pages (activé si on se trouve sur la page correspondante) -->
             <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
-                <a href="http://localhost:8000/.List.php/?page=<?= $page ?>" class="page-link"><?= $page ?></a>
+                <a href="list.php/?page=<?= $page ?>" class="page-link"><?= $page ?></a>
             </li>
         <?php endfor ?>
             <!-- Lien vers la page suivante (désactivé si on se trouve sur la dernière page) -->
             <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
-            <a href="http://localhost:8000/.List.php/?page=<?= $currentPage + 1 ?>" class="page-link">Suivante</a>
+            <a href="list.php/?page=<?= $currentPage + 1 ?>" class="page-link">Suivante</a>
         </li>
     </ul>
 </nav>
